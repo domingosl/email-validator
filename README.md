457bf2eecce1a2307ae31b60366156a283cfd08d
Domingo <ing.domingos.l@gmail.com>
2023-05-21 19:47:43 +0200

    Clean up

---
+++ README.md
@@ -0,0 +1,36 @@
+# 0xAI
+Get meaningful insights from your email addresses thanks to AI.
+
+With ❤ From Milan 🍕, By Eng. Domingo Lupo
+
+-----
+
+### HOW TO INSTALL (NodeJS +16 is required)
+
+- Clone the repo
+- Create a .env using the .env.example as guide
+- run ```npm i```
+
+You'll need a OpenAI api key.
+
+-----
+
+### HOW TO RUN
+
+#### SINGLE EMAIL VALIDATION
+```node index -e email```
+
+```node index -e email --useAI``` for AI support
+
+#### BATCH EMAIL VALIDATION
+```node index -i filename```
+
+```node index -i filename --useAI``` for AI support
+
+
+-----
+
+#### For more information:
+
+- Devpost page:
+- Video presentation:
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const os = require('os');
+require('dotenv').config();
+const killProcess = require('./lib/kill-process');
+const storage = require('./lib/storage');
+const report = require('./lib/report');
+
+if(!process.env.OPENAI_ORGANIZATION_ID || !process.env.OPENAI_SECRET_KEY)
+    killProcess("OPENAI_ORGANIZATION_ID and OPENAI_SECRET_KEY env variables should be set in .env");
+
 const fs = require('fs');
-
-const systemCpuCores = os.cpus().length;
-
 
 const validate = require('./lib/validate');
 
 const optionDefinitions = [
     { name: 'input', alias: 'i', type: String },
     { name: 'email', alias: 'e', type: String },
-    { name: 'method', alias: 'm', type: String }
+    { name: 'method', alias: 'm', type: String },
+    { name: 'useAI', alias: 'a', type: Boolean }
 ]
 
 const commandLineArgs = require('command-line-args');
@@ -20,28 +25,62 @@ try {
     options = commandLineArgs(optionDefinitions);
 }
 catch (error) {
-    console.log(error.message) && process.exit();
+    killProcess(error.message);
 }
 
 if(options.input && options.email)
-    console.log('Either email or file should be pass, not both!') && process.exit();
+    killProcess('Either email or file should be pass, not both!');
 
 (async function process() {
 
+    const startDatetime = new Date().getTime();
+    let promises, statusTimer;
+
     if (options.email)
-        console.log(await validate(options.email));
+        promises = [await validate.process(options.email, options.useAI)];
 
     else if(options.input) {
 
+        console.log("Loading file into memory and cleaning it up!");
+
         const data = fs.readFileSync(options.input).toString().split("\n");
-        const cleanData = [];
-        data.forEach(line => {
-            cleanData.push(line.split(",")[0].trim());
+
+        statusTimer = setInterval(()=>console.log("Batch validation process running..."), 1000);
+
+        promises = data.map(async (line, index) => {
+            //Ignoring duplicates
+            if(data.indexOf(line) !== index)
+                return;
+
+            await validate.process(line.split(",")[0].trim(), options.useAI);
         });
 
-        cleanData.forEach(async email => console.log(await validate(email)));
-
     }
+
+    Promise.all(promises).then(async ()=>{
+
+        const { mxFounds, smtpFails, count, totalOk, aiResults, aiCacheHits, results } = validate.getSessionCache();
+        const processTime = (new Date().getTime() - startDatetime);
+        clearInterval(statusTimer);
+
+        console.log("DONE. Emails checked:", count, "Time[ms]:", processTime, "Total valid:", totalOk);
+
+        if(options.useAI)
+            console.log("AI Cache hits:", aiCacheHits);
+
+        if(options.email)
+            console.log(results[0]);
+
+        storage.write('mx-founds.json', mxFounds);
+        storage.write('smtp-fails.json', smtpFails);
+        storage.write('ai-results.json', aiResults);
+
+        console.log("Generating report...");
+
+        const file = await report.generate(results, count, totalOk);
+
+        console.log("Report available at: ", file);
+    });
 
 
 })();
